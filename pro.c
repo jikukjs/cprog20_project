b909a165eb7e0ce9f573e7842389adea24ff34de
jikukjs <jikukjs7@gmail.com>
2020-12-11 21:48:54 +0900

    pro.c added

--- pro.c
+++ pro.c
@@ -12,13 +12,17 @@ typedef struct music
 	struct music* next;
 } My_p;
 
-My_p* list_head = NULL;
 
-void Add (My_p* list_head);
+My_p* Check(My_p* list_head,char* name);
+void Add (My_p** list_head, My_p*(*func)(My_p*,char*));
+void Look (My_p* list_head);
+void Delete (My_p* list_head, My_p*(*func)(My_p*,char*));
+
 
 int main()
 {
 	int input;
+	My_p* list_head = NULL;
 
 	printf("나의 플레이리스트");	
 
@@ -47,6 +51,7 @@ int main()
 		else if (input ==1)
 		{
 			printf("\n[플레이리스트 보기]\n\n");	
+			Look(list_head);
 		}
         
 		else if (input ==2)
@@ -66,12 +71,13 @@ int main()
 		else if (input ==5)
 		{
 			printf("\n[노래 추가하기]\n\n");
-			Add(list_head);
+			Add(&list_head,Check);
 		}
         
         else if (input == 6)
 		{
             printf("\n[노래 삭제하기]\n\n");
+			Delete(list_head, Check);
         }
 		
 		else if (input == 7)
@@ -97,34 +103,115 @@ int main()
     }
 }	
 
-void Add(My_p* list_head)
+My_p* Check(My_p* list_head, char* name)
+{
+	My_p* tmp=list_head;
+	while(tmp)
+	{
+		if(strcmp(tmp->title, name)==0)
+			return tmp;
+		tmp=tmp->next;
+	}
+	return tmp;
+}
+
+
+void Add(My_p** list_head, My_p*(*func)(My_p*,char* ))
 {
 	My_p* new_node;
+	My_p* check_node;
+
 	char new_title[10];
 	char new_singer[10];
 	char new_genre[10];
 	int new_like;
 	int new_cnt = 0;
-	
+	char name[20];
+		
 	new_node = (My_p*)malloc (sizeof(My_p));
 	
-	printf("음악 제목 : ");
-	scanf("%s", new_title);
-	printf("가수 : ");
-	scanf("%s", new_singer);
-	printf("장르 : ");
-	scanf("%s", new_genre);
-	printf("좋아요(1) / 그 외(0) :");
-	scanf("%d", &new_like);
-	
-	strcpy(new_node -> title , new_title);
-	strcpy(new_node -> singer , new_singer);
-	strcpy(new_node -> genre , new_genre);
-	new_node -> like = new_like;
-	new_node -> play_cnt = new_cnt;
+	printf("노래 유무 확인 :");
+	scanf("%s",name);
+	check_node = func(*list_head,name);
 
-	new_node -> next = list_head;
-	list_head = new_node;
+	if(check_node)
+	{
+		printf("이미 존재하는 곡입니다.\n");
+	}
+	else
+	{
+		printf("\n추가하실 곡의 정보를 입력해주세요\n");
+		printf("곡 제목 : ");
+		scanf("%s", new_title);
+		printf("가수 : ");
+		scanf("%s", new_singer);
+		printf("장르 : ");
+		scanf("%s", new_genre);
+		printf("좋아요(1) / 그 외(0) :");
+		scanf("%d", &new_like);
+	
+		strcpy(new_node -> title , new_title);
+		strcpy(new_node -> singer , new_singer);
+		strcpy(new_node -> genre , new_genre);
+		new_node -> like = new_like;
+		new_node -> play_cnt = new_cnt;
+	
+		new_node -> next =*list_head;
+		*list_head = new_node;
+	}
 }
 
+void Look(My_p* list_head)
+{	
+	My_p* tmp = list_head;
+
+	while(tmp != NULL)
+	{
+		printf("%s - ", tmp->title);
+		printf("%s - ", tmp->singer);
+		printf("%s - ", tmp->genre);
+		printf("좋아요 : %d\n", tmp->like);
+		tmp = tmp -> next;
+	}
+}
+
+void Delete(My_p* list_head, My_p*(*func)(My_p*,char*))
+{
+	My_p* check_node;
+	My_p* pre = list_head;
+	My_p* now = pre->next;
+
+	char name[30];
+	int del_choice;
+	int num=0;
+	printf("삭제할 노래 : ");
+	scanf("%s",name);
+	check_node=func(list_head,name);
+	if(check_node==NULL)
+	{
+		printf("존재하지 않는 옷입니다.\n");
+	}
+
+	else
+	{
+		while (now != NULL)
+		{
+			if(strcmp(list_head->title, check_node->title)==0)
+			{
+				list_head = list_head->next;
+				free(list_head);
+				break;
+			}
+			else if (strcmp(now->title, name)==0)
+			{
+				pre->next = now->next;
+				free(now);
+				break;
+			}
+			pre = now;
+			now = now->next;
+		}
+	}
+
+}
 
